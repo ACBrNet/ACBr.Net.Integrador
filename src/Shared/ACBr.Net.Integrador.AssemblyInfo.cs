f7518d71ee331515d5986b1eb8c329540cab637a
RFTD <rfteno@gmail.com>
2019-01-05 22:30:56 -0300

    [*] Atualização de dependências .

--- src/Shared/ACBr.Net.Integrador.AssemblyInfo.cs
+++ src/Shared/ACBr.Net.Integrador.AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("ACBr.Net Integrador")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ACBr.Net")]
-[assembly: AssemblyCopyright("Copyright © Grupo ACBr.Net 2014 - 2018")]
+[assembly: AssemblyCopyright("Copyright © Grupo ACBr.Net 2014 - 2019")]
 [assembly: AssemblyTrademark("Grupo ACBr.Net https://acbrnet.github.io")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.2.1")]
-[assembly: AssemblyFileVersion("1.0.2.1")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
